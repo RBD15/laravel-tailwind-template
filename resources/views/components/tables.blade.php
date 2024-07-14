7bea9a57acccc7130145762ba60dc58f65b25676
Roberto D <roberto.dopazo15@gmail.com>
2024-07-14 15:31:18 -0400

    [EDIT] table component, first step to create table dynamically

--- resources/views/components/tables.blade.php
+++ resources/views/components/tables.blade.php
@@ -1,3 +1,99 @@
+<?php
+$data = '[
+    {
+        "OrderID": "#OR7546",
+        "CustomerName": "Harold Hobbs",
+        "Price": "$ 123.00",
+        "Status": "Paid",
+        "Date": "Sep 30/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR4359",
+        "CustomerName": "Shirley Williams",
+        "Price": "$ 745.00",
+        "Status": "Shipped",
+        "Date": "Sep 28/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR3654",
+        "CustomerName": "Timothy Reyes",
+        "Price": "$ 1344.00",
+        "Status": "Pending",
+        "Date": "Sep 27/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR7451",
+        "CustomerName": "Jane Stillman",
+        "Price": "$ 230.00",
+        "Status": "Canceled",
+        "Date": "Sep 24/2021",
+        "ViewDetails": "View Details "
+    },
+    {
+        "OrderID": "#OR1324",
+        "CustomerName": "Leon Blank",
+        "Price": "$ 511.00",
+        "Status": "Shipped",
+        "Date": "Sep 20/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR5331",
+        "CustomerName": "Cecilia Hendric",
+        "Price": "$ 2654.00",
+        "Status": "Paid",
+        "Date": "Sep 09/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR1874",
+        "CustomerName": "Suzanne Williams",
+        "Price": "$ 400.00",
+        "Status": "Paid",
+        "Date": "Aug 31/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR4963",
+        "CustomerName": "Gerland Falvin",
+        "Price": "$ 954.00",
+        "Status": "Pending",
+        "Date": "Aug 31/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR9452",
+        "CustomerName": "Crystal Reagle",
+        "Price": "$ 1365.00",
+        "Status": "Canceled",
+        "Date": "Aug 25/2021",
+        "ViewDetails": "View Details"
+    },
+    {
+        "OrderID": "#OR8563",
+        "CustomerName": "John Reeves",
+        "Price": "$ 164.00",
+        "Status": "Shipped",
+        "Date": "July 20/2021",
+        "ViewDetails": "View Details"
+    }
+]';
+$dataJson = json_decode($data);
+
+function getColumns($object){
+    $columns = [];
+    $objectArray = get_object_vars($object);
+    foreach ($objectArray as $key => $value) {
+        $columns[] = $key;
+    }
+    return $columns;
+}
+$columns = getColumns($dataJson[0]);
+?>
+
 <!-- start::Table -->
 <div class="bg-white rounded-lg px-8 py-6 overflow-x-scroll custom-scrollbar">
     <div class="flex justify-between">
@@ -9,26 +105,35 @@
         <thead class="bg-secondary text-gray-100 font-bold">
             <td>
             </td>
-            <td class="py-2 pl-2">
-                Order ID
-            </td>
-            <td class="py-2 pl-2">
-                Customer Name
-            </td>
-            <td class="py-2 pl-2">
-                Price
-            </td>
-            <td class="py-2 pl-2">
-                Status
-            </td>
-            <td class="py-2 pl-2">
-                Date
-            </td>
-            <td class="py-2 pl-2">
-                View Details
-            </td>
+            @foreach ($columns as $columnName)
+                <td class="py-2 pl-2">
+                    {{$columnName}}
+                </td>
+            @endforeach
         </thead>
         <tbody class="text-sm">
+            @foreach ($dataJson as $object)
+                <tr class="bg-gray-100 hover:bg-primary hover:bg-opacity-20 transition duration-200">
+                    <td class="py-3 pl-2">
+                        <input type="checkbox" class="rounded focus:ring-0 checked:bg-red-500 ml-2">
+                    </td>
+                    @foreach ($object as $key => $value)
+                        @if ($key === 'Status')
+                            <td class="py-3 pl-2">
+                                <span class="bg-green-500 px-1.5 py-0.5 rounded-lg text-gray-100">{{$value}}</span>
+                            </td>
+                        @elseif ($key === 'ViewDetails')
+                            <td class="py-3 pl-2">
+                                <a href="#" class="bg-primary hover:bg-opacity-90 px-2 py-1 mr-2 text-gray-100 rounded-lg">View Details</a>
+                            </td>
+                        @else
+                            <td class="py-3 pl-2">
+                                {{$value}}
+                            </td>
+                        @endif
+                    @endforeach
+                </tr>
+            @endforeach
             <tr class="bg-gray-100 hover:bg-primary hover:bg-opacity-20 transition duration-200">
                 <td class="py-3 pl-2">
                     <input type="checkbox" class="rounded focus:ring-0 checked:bg-red-500 ml-2">
@@ -52,7 +157,7 @@
                     <a href="#" class="bg-primary hover:bg-opacity-90 px-2 py-1 mr-2 text-gray-100 rounded-lg">View Details</a>
                 </td>
             </tr>
-            <tr class="bg-gray-200 hover:bg-primary hover:bg-opacity-20 transition duration-200">
+            {{-- <tr class="bg-gray-200 hover:bg-primary hover:bg-opacity-20 transition duration-200">
                 <td class="py-3 pl-2">
                     <input type="checkbox" class="rounded focus:ring-0 checked:bg-red-500 ml-2">
                 </td>
@@ -258,7 +363,7 @@
                 <td class="py-3 pl-2">
                     <a href="#" class="bg-primary hover:bg-opacity-90 px-2 py-1 mr-2 text-gray-100 rounded-lg">View Details</a>
                 </td>
-            </tr>
+            </tr> --}}
         </tbody>
     </table>
 </div>
